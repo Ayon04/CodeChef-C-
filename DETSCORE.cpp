93776124786aed577fe58e737f42af3865a3aeaf
Ayon Ghosh <103620492+Ayon04@users.noreply.github.com>
2024-06-04 21:44:43 +0600

    Add files via upload

---
+++ DETSCORE.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	int n,a,b,ans;
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+	    cin>>a>>b;
+	    
+	    ans=(a/10)*b;
+	    
+	    cout<<ans<<endl;
+	    
+	}
+
+}
